1c6f0f45a76ae7d98599dae7076bfb8eefa2c6fa
alineayumiw <aline.ayumiw@gmail.com>
2023-06-22 03:33:57 +0900

    added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Aline's Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Aline's Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Aline Watanabe</footer>
+      </div>
     </div>
   );
 }
