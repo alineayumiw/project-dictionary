f8174581777078f07931a6725da7c3e9b430d5ae
alineayumiw <aline.ayumiw@gmail.com>
2023-06-21 15:18:17 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.jpg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Aline's Dictionary</h1>
+      </header>
+    </div>
+  );
+}
