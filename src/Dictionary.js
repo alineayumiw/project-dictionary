6dc5c9b0d086decf64a34c479e146615b44ad977
alineayumiw <aline.ayumiw@gmail.com>
2023-06-23 04:16:11 +0900

    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyWord}`);
+
+    let apiKey = "133a3ft3fab6094b0b7471o16009a6a4";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
